0f57e4d3c3b01407fe9c6c0e38a2828962247b91
satisfy <104036234+satisfyu@users.noreply.github.com>
2025-03-13 11:49:53 +0100

    feat(port): porting to 1.21.

--- common/src/main/java/net/satisfy/farm_and_charm/core/util/FarmAndCharmIdentifier.java
+++ common/src/main/java/net/satisfy/farm_and_charm/core/util/FarmAndCharmIdentifier.java
@@ -3,10 +3,13 @@ package net.satisfy.farm_and_charm.core.util;
 import net.minecraft.resources.ResourceLocation;
 import net.satisfy.farm_and_charm.FarmAndCharm;
 
-public class FarmAndCharmIdentifier extends ResourceLocation {
+public final class FarmAndCharmIdentifier {
 
-    public FarmAndCharmIdentifier(String path) {
-        super(FarmAndCharm.MOD_ID, path);
+    public static ResourceLocation of(String path) {
+        return ResourceLocation.fromNamespaceAndPath(FarmAndCharm.MOD_ID, path);
     }
 
+    public static ResourceLocation of(String namespace, String path) {
+        return ResourceLocation.fromNamespaceAndPath(namespace, path);
+    }
 }

bbf5cf2a6850a2e2c94f471ba7461a560e6e85d5
MisledWater79 <59891258+MisledWater79@users.noreply.github.com>
2026-01-10 08:37:27 -0500

    Fix for Fertilized Farmland 1.21.1 (#36)
    * fix(): Fertilized Farmland not bonemealing
    
    * feat()/fix(): Added getGrowthSpeed to CropBlockMixin

--- neoforge/src/main/java/net/satisfy/farm_and_charm/neoforge/core/mixin/CropBlockMixin.java
+++ neoforge/src/main/java/net/satisfy/farm_and_charm/neoforge/core/mixin/CropBlockMixin.java
@@ -1,12 +1,17 @@
 package net.satisfy.farm_and_charm.neoforge.core.mixin;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.FarmBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.util.TriState;
 import net.satisfy.farm_and_charm.core.registry.ObjectRegistry;
 import net.satisfy.farm_and_charm.platform.PlatformHelper;
 import org.spongepowered.asm.mixin.Mixin;
@@ -49,5 +54,60 @@ public abstract class CropBlockMixin {
         if (blockState.is(ObjectRegistry.FERTILIZED_FARM_BLOCK.get())) {
             cir.setReturnValue(true);
         }
+    }
+
+    @Inject(method = "getGrowthSpeed", at = @At("HEAD"), cancellable = true)
+    private static void getGrowthSpeed(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CallbackInfoReturnable<Float> cir) {
+        Block block = blockState.getBlock();
+        float value = 1.0F;
+        BlockPos below = blockPos.below();
+
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                float g = 0.0F;
+
+                label: {
+                    BlockState blockState2 = blockGetter.getBlockState(below.offset(i, 0, j));
+                    TriState soilDecision = blockState2.canSustainPlant(blockGetter, below.offset(i, 0, j), Direction.UP, blockState);
+                    if (soilDecision.isDefault()) {
+                        if (!(blockState2.getBlock() instanceof FarmBlock)) {
+                            break label;
+                        }
+                    } else if (!soilDecision.isTrue()) {
+                        break label;
+                    }
+
+                    g = 1.0F;
+                    if (blockState2.isFertile(blockGetter, blockPos.offset(i, 0, j))) {
+                        g = 3.0F;
+                    } else if (blockState.is(ObjectRegistry.FERTILIZED_FARM_BLOCK.get())) {
+                        g = 4.0F;
+                    }
+                }
+
+                if (i != 0 || j != 0) {
+                    g /= 4.0F;
+                }
+
+                value += g;
+            }
+        }
+
+        BlockPos north = blockPos.north();
+        BlockPos south = blockPos.south();
+        BlockPos west = blockPos.west();
+        BlockPos east = blockPos.east();
+        boolean isFertilized1 = blockGetter.getBlockState(west).is(block) || blockGetter.getBlockState(east).is(block);
+        boolean isFertilized2 = blockGetter.getBlockState(north).is(block) || blockGetter.getBlockState(south).is(block);
+        if (isFertilized1 && isFertilized2) {
+            value /= 2.0F;
+        } else {
+            boolean isFertilized3 = blockGetter.getBlockState(west.north()).is(block) || blockGetter.getBlockState(east.north()).is(block) || blockGetter.getBlockState(east.south()).is(block) || blockGetter.getBlockState(west.south()).is(block);
+            if (isFertilized3) {
+                value /= 2.0F;
+            }
+        }
+
+        cir.setReturnValue(value);
     }
 }

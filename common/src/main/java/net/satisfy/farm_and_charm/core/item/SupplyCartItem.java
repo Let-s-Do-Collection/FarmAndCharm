449562ce93dc554550e17bd9466959048b7d98cc
satisfy <104036234+satisfyu@users.noreply.github.com>
2026-01-27 12:51:24 +0100

    feat(cart): Removed FQN

--- common/src/main/java/net/satisfy/farm_and_charm/core/item/SupplyCartItem.java
+++ common/src/main/java/net/satisfy/farm_and_charm/core/item/SupplyCartItem.java
@@ -3,6 +3,7 @@ package net.satisfy.farm_and_charm.core.item;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextColor;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.satisfy.farm_and_charm.core.registry.EntityTypeRegistry;
 
@@ -14,7 +15,7 @@ public class SupplyCartItem extends AbstractCartItem {
     }
 
     @Override
-    protected net.minecraft.world.entity.Entity createCartEntity(Level level) {
+    protected Entity createCartEntity(Level level) {
         return EntityTypeRegistry.SUPPLY_CART.get().create(level);
     }
 
